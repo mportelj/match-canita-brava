cf4f2c0f3429c548dc9bb63a607d32807fa22e55
Manuel Portela <111420296+mportelj@users.noreply.github.com>
2026-05-07 16:48:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ import gspread
 # Función para conectar sin usar st.connection
 def cargar_datos_golf():
     # 1. Cargamos los secretos
-    s = st.secrets["gsheets"]
+    s = st.secrets["golf_datos"]
     
     # 2. Construimos el diccionario de credenciales
     # El .replace garantiza que los saltos de línea sean correctos
