e793aa8413b98293aba05fd8648f24afe368d444
Manuel Portela <111420296+mportelj@users.noreply.github.com>
2026-05-09 20:48:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -472,14 +472,14 @@ s1_val = col_j1.number_input(TODOS[1], 1, 15, v_ref[1], key=f"in_s1_{h}_{st.sess
 s2_val = col_j2.number_input(TODOS[2], 1, 15, v_ref[2], key=f"in_s2_{h}_{st.session_state.refresco_id}")
 s3_val = col_j2.number_input(TODOS[3], 1, 15, v_ref[3], key=f"in_s3_{h}_{st.session_state.refresco_id}")
 
-        # --- BLOQUE G: ACCIÓN DE GUARDADO ---
-        if st.button("💾 Guardar Hoyo", type="primary", use_container_width=True):
+# --- BLOQUE G: ACCIÓN DE GUARDADO ---
+if st.button("💾 Guardar Hoyo", type="primary", use_container_width=True):
             st.toast("⏳ Iniciando guardado...", icon="⏳")
             ejecutar_guardado_automatico(h, s0_val, s1_val, s2_val, s3_val)
             st.rerun()
 
-        # --- BLOQUE H: FINALIZAR ---
-        st.write("---") 
+# --- BLOQUE H: FINALIZAR ---
+st.write("---") 
         
         with st.popover("🏁 Finalizar Partida", use_container_width=True):
             st.warning("¿Estás seguro de que quieres cerrar la partida actual?")
