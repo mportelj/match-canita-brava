41c0f3302e2d8054a9c4e74d69627185a495c81b
Manuel Portela <111420296+mportelj@users.noreply.github.com>
2026-05-09 12:03:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -293,14 +293,7 @@ if df_raw is not None and not df_raw.empty:
 if st.session_state.menu_seleccionado == "Inicio":
     st.title("⛳ CAÑITA BRAVA")
     df = leer_datos()
-#-----------
-try:
-    # Intenta escribir la hora en una celda lejana (Z1)
-    sh.update_acell('Z1', f"Last Sync: {datetime.now().strftime('%H:%M:%S')}")
-    st.sidebar.success(f"Conectado a: {sh.title}")
-except Exception as e:
-    st.sidebar.error(f"Error de enlace: {e}")
-#-------
+
     
     # Definimos la temporada actual
     anio_actual = 2026
