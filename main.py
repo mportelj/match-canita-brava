932e20d805b3c337598f3135814bd76f5e997c5c
Manuel Portela <111420296+mportelj@users.noreply.github.com>
2026-05-09 11:10:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -309,7 +309,7 @@ if st.session_state.menu_seleccionado == "Inicio":
 # ==========================================
 # SECCIÓN: NUEVA PARTIDA (Modo Match Play)
 # ==========================================
-elif st.session_state.menu_seleccionado == "Jugar/Editar":
+elif st.session_state.menu_seleccionado == "Nueeva Partida":
     if 'refresco_id' not in st.session_state: 
         st.session_state.refresco_id = 0
 
