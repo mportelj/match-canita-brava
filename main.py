b08ad08c85180d0ccbdf566dbb351d4b30090357
Manuel Portela <111420296+mportelj@users.noreply.github.com>
2026-07-08 11:26:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -328,7 +328,7 @@ def calcular_bonus_hoyo(golpes_jugador, par):
 
         
 
-def ejecutar_guardado_automatico(hoyo_id, g0, g1, g2, g3):
+def ejecutar_guardado_automatico(hoyo_id, g0, g1, g2, g3, p0, p1, p2, p3):
     try:
         hoja = st.session_state.sh
         g = st.session_state.game
