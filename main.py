500ebba61baa2a6b0e257024607f27cf4c035d6b
Manuel Portela <111420296+mportelj@users.noreply.github.com>
2026-07-16 08:39:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -169,7 +169,7 @@ with st.sidebar:
         index=idx_actual,
         key="nav_radio",
         on_change=cambiar_menu
-         )
+        )
 # Calculamos el índice basándonos en el texto guardado (mantenido por si lo usas en el resto del código)
 try:
     idx_actual = opciones_menu.index(st.session_state.menu_seleccionado)
